051b2e306815a4e917a8f0629dd190215afbee32
hiro <hironori.matsuse@gmail.com>
2019-10-09 13:54:24 +0900

    Add:ログインシステムの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :pictures do
     collection do
       post :confirm
     end
   end
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
 end
